082a508b665e221e8a441f6db7b7ef51766a746e
carlosmendoza <ca.mendoza@uniandes.edu.co>
2019-09-18 18:50:47 -0500

    visualizacion

--- src/components/Busqueda.js
+++ src/components/Busqueda.js
@@ -1,48 +1,47 @@
 import React from 'react';
 import '../App.css';
-import navio from "navio";
+import Navio from "./Navio";
 
 
 class Busqueda extends React.Component {
 
   onChange()
   {
-    this.setState({'ruta':this.myIn.value})
+    let url = document.getElementsByName('url')[0].value
+    console.log("a",url)
+    this.setState({'url':url})
   }
   constructor(props)
   {
     super(props)
-    this.setState({ruta:'', datos:[]});
+    this.state ={url:'', datos:[]}
+    this.metodoNavio = this.metodoNavio.bind(this);
     this.onChange = this.onChange.bind(this);
-    this.metodoAux = this.metodoAux.bind(this);
+
 
   }
-  metodoAux()
-  {
-    var nv = navio(this.myIn.value,600);
-    var dataAux = this.s
-    nv.data(data);
-    nv.addAllAtribs()
-  }
+
   metodoNavio()
   {
-    var url = this.state['ruta']
+
+    let url = document.getElementsByName('url')[0].value
+    console.log("metodo navio", url)
     fetch(url)
       .then(res => res.json())
       .then(data => {
           console.log(data)
-          this.state({'data':data})
+          this.setState({'data':data})
 
-      }, this.metodoAux());
+      });
 
   }
   render()
   {
       return(<div>
-        <input type="text">PRUEBA
-        rfef={myIn => this.myIn=myIn}
-        onInput ={this.onChange.bind}</input>
+        <input type="text" name="url" onChange={this.onChange}>
+        </input>
         <button onClick={this.metodoNavio}>ACEPTAR</button>
+        <Navio data={this.state['data']}></Navio>
       </div>);
   }
 }
--- src/components/Navio.js
+++ src/components/Navio.js
@@ -1,14 +1,33 @@
 import navio from "navio";
+import React from 'react'
 
+class Navio extends React.Component
+{
+constructor(props)
+{
+  super(props);
+  this.state={};
+}
+componentDidUpdate()
+{
+    let dataAux = this.props.data
+    let nv = navio(this.myDiv,600);
+    console.log(dataAux)
+    if(dataAux!==undefined)
+    {
+    console.log(dataAux)
+    nv.data(dataAux);
+    nv.addAllAttribs();
+    }
+}
 render ()
 {
-  update()
-  {
-    //new (this.navio)
-  }
+  
   return(
     <div ref={myDiv => this.myDiv=myDiv}>
 
     </div>
   )
 }
+}
+export default Navio
