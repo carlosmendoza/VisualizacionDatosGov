dab1c9ead6d3bcc9ed7b46be3d2e876e1cdf4352
carlosmendoza <ca.mendoza@uniandes.edu.co>
2019-09-19 03:36:32 -0500

    adicion creativa

--- routes/historial.js
+++ routes/historial.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/consulta", (req, res) => {
+  req.db
+    .collection("historialCollection")
+    .aggregate([
+      {"$group" : {_id:"$url", count:{$sum:1}}}
+    ])
+    .toArray((err, result) => {
+      if (err) res.status(500).json({ message: err.message });
+
+      res.json(result);
+    });
+});
+
 router.get("/", (req, res) => {
   req.db
     .collection("historialCollection")
@@ -11,6 +24,8 @@ router.get("/", (req, res) => {
       res.json(result);
     });
 });
+
+
 
 router.post("/", (req, res) => {
     req.db.collection("historialCollection").insert(req.body, (err, result) => {
